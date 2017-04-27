a3cdd06c664ef62fbb736368b1b822782037abb1
zengl <zengl@tzg.cn>
2017-04-27 18:22:27 +0800

    clean project

--- src/main/java/com/tzg/webapp/auth/resource/ResourceService.java
+++
@@ -1,9 +0,0 @@
-package com.tzg.webapp.auth.resource;
-
-import com.tzg.service.support.proto.ProtoService;
-
-public interface ResourceService extends ProtoService< Resource > {
-
-    void deleteNode( Integer id ) throws Exception;
-
-}
--- src/main/java/com/tzg/webapp/auth/role/RoleMapper.java
+++
@@ -1,7 +0,0 @@
-package com.tzg.webapp.auth.role;
-
-import com.tzg.service.support.proto.ProtoMapper;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleMapper extends ProtoMapper< Role > { }
