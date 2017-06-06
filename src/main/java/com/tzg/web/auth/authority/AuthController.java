cbfa0b15616b9b5750cf3a0a796fca546c68d916
zenglin <zengl@tzg.cn>
2017-06-06 12:13:36 +0800

    fix bug

--- src/main/java/com/tzg/web/auth/authority/AuthController.java
+++ src/main/java/com/tzg/web/auth/authority/AuthController.java
@@ -1,23 +1,18 @@
 package com.tzg.web.auth.authority;
 
-import com.tzg.service.support.json.JsonResp;
 import com.tzg.web.auth.authorization.Authorization;
-import com.tzg.web.auth.permission.Permission;
-import com.tzg.web.auth.user.User;
 import com.tzg.web.auth.authorization.AuthorizationService;
+import com.tzg.web.auth.permission.Permission;
 import com.tzg.web.auth.permission.PermissionService;
 import com.tzg.web.auth.resource.ResourceService;
+import com.tzg.web.auth.user.User;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
@@ -28,9 +23,9 @@ import java.util.*;
 @RequestMapping( "/auth" )
 public class AuthController {
 
-    private static final String LOGIN_PAGE        = "../../login";     // 登录页面地址
-    private static final String INDEX_PAGE        = "../../index";     // 登录后的首页
-    private static final String UNAUTHORIZED_PAGE = "../../401";       // 未授权页面
+    private static final String LOGIN_PAGE        = "login";     // 登录页面地址
+    private static final String INDEX_PAGE        = "index";     // 登录后的首页
+    private static final String UNAUTHORIZED_PAGE = "401";       // 未授权页面
 
     @Resource
     private ResourceService resourceService;
