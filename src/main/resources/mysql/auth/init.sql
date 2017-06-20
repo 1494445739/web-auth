f55db43d81a07b139face22952ca7642e3bad8c5
zenglin <zengl@tzg.cn>
2017-06-20 15:44:41 +0800

    fix bug

--- src/main/resources/mysql/auth/init.sql
+++ src/main/resources/mysql/auth/init.sql
@@ -7,7 +7,8 @@ DROP TABLE IF EXISTS AUTH_AUTHORIZATION;
 # 用户表。 CDT - 创建日期时间； UDT - 更新日期时间。以下略
 CREATE TABLE IF NOT EXISTS AUTH_USER (
   ID       INT(10)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  NAME     VARCHAR(30)  NOT NULL COMMENT '用户名',
+  NAME     VARCHAR(30)  NOT NULL
+  COMMENT '用户名',
   PASSWORD VARCHAR(100) NOT NULL,
   CDT      DATETIME     NOT NULL,
   UDT      DATETIME     NOT NULL,
@@ -82,40 +83,41 @@ INSERT INTO `auth_role` VALUES (1, '系统管理员', now( ), now( ));
 
 # 初始化auth_resource表
 INSERT INTO `auth_resource`
-VALUES (1, 0, '系统管理', 'd', 1, '', now( ), now( )), (2, 1, '权限管理', 'm', 1, '/auth/main', now( ), now( )),
-  (46, 2, '角色查询', 'a', NULL, '/auth/role/proto/index', now( ), now( )),
-  (47, 2, '资源查询', 'a', NULL, '/auth/resource/proto/index', now( ), now( )),
-  (48, 2, '用户查询', 'a', NULL, '/auth/user/proto/index', now( ), now( )),
-  (49, 2, '用户添加', 'a', NULL, '/auth/user/proto/post', now( ), now( )),
-  (50, 2, '角色添加', 'a', NULL, '/auth/role/proto/post', now( ), now( )),
-  (51, 2, '资源添加', 'a', NULL, '/auth/resource/proto/post', now( ), now( )),
-  (52, 2, '用户查看', 'a', NULL, '/auth/user/proto/get', now( ), now( )),
-  (53, 2, '角色查看', 'a', NULL, '/auth/role/proto/get', now( ), now( )),
-  (54, 2, '资源查看', 'a', NULL, '/auth/resource/proto/get', now( ), now( )),
-  (55, 2, '用户修改', 'a', NULL, '/auth/user/proto/put', now( ), now( )),
-  (56, 2, '角色修改', 'a', NULL, '/auth/role/proto/put', now( ), now( )),
-  (57, 2, '资源修改', 'a', NULL, '/auth/resource/proto/put', now( ), now( )),
-  (58, 2, '用户删除', 'a', NULL, '/auth/user/proto/delete', now( 角色分配资源), now( )),
-  (59, 2, '角色删除', 'a', NULL, '/auth/role/proto/delete', now( ), now( )),
-  (60, 2, '资源删除', 'a', NULL, '/auth/resource/proto/delete', now( ), now( )),
-  (61, 2, '用户批量删除', 'a', NULL, '/auth/user/proto/deleteList', now( ), now( )),
-  (62, 2, '角色批量删除', 'a', NULL, '/auth/role/proto/deleteList', now( ), now( )),
+VALUES (1, 0, '系统管理', 'd', 1, '', now( ), now( )), (46, 70, '角色查询', 'a', NULL, '/auth/role/proto/index', now( ), now( )),
+  (47, 71, '资源查询', 'a', NULL, '/auth/resource/proto/index', now( ), now( )),
+  (48, 69, '用户查询', 'a', NULL, '/auth/user/proto/index', now( ), now( )),
+  (49, 69, '用户添加', 'a', NULL, '/auth/user/proto/post', now( ), now( )),
+  (50, 70, '角色添加', 'a', NULL, '/auth/role/proto/post', now( ), now( )),
+  (51, 71, '资源添加', 'a', NULL, '/auth/resource/proto/post', now( ), now( )),
+  (52, 69, '用户查看', 'a', NULL, '/auth/user/proto/get', now( ), now( )),
+  (53, 70, '角色查看', 'a', NULL, '/auth/role/proto/get', now( ), now( )),
+  (54, 71, '资源查看', 'a', NULL, '/auth/resource/proto/get', now( ), now( )),
+  (55, 69, '用户修改', 'a', NULL, '/auth/user/proto/put', now( ), now( )),
+  (56, 70, '角色修改', 'a', NULL, '/auth/role/proto/put', now( ), now( )),
+  (57, 71, '资源修改', 'a', NULL, '/auth/resource/proto/put', now( ), now( )),
+  (58, 69, '用户删除', 'a', NULL, '/auth/user/proto/delete', now( ), now( )),
+  (59, 70, '角色删除', 'a', NULL, '/auth/role/proto/delete', now( ), now( )),
+  (60, 71, '资源删除', 'a', NULL, '/auth/resource/proto/delete', now( ), now( )),
+  (61, 69, '用户批量删除', 'a', NULL, '/auth/user/proto/deleteList', now( ), now( )),
+  (62, 70, '角色批量删除', 'a', NULL, '/auth/role/proto/deleteList', now( ), now( )),
   (63, 2, '分配角色查询', 'a', NULL, '/auth/authz/get', now( ), now( )),
   (64, 2, '分配资源查询', 'a', NULL, '/auth/permission/get', now( ), now( )),
-  (66, 2, '用户分配角色', 'a', NULL, '/auth/authz/authz', now( ), now( )),
-  (67, 2, '', 'a', NULL, '/auth/permission/authz', now( ), now( )),
-  (68, 2, '角色授权', 'a', NULL, '/auth/authz', now( ), now( ));
+  (66, 69, '用户分配角色', 'a', NULL, '/auth/authz/authz', now( ), now( )),
+  (67, 70, '角色分配资源', 'a', NULL, '/auth/permission/authz', now( ), now( )),
+  (68, 70, '角色授权', 'a', NULL, '/auth/authz', now( ), now( )), (69, 1, '用户管理', 'm', 1, '/auth/user', now( ), now( )),
+  (70, 1, '角色管理', 'm', 2, '/auth/role', now( ), now( )), (71, 1, '资源管理', 'm', 3, '/auth/resource', now( ), now( ));
 
 # 初始化auth_authorization
 INSERT INTO `auth_authorization` VALUES (17, 20, 1, now( ), now( ));
 
 # 初始化auth_permission
 INSERT INTO `auth_permission`
-VALUES (234, 1, 1, now( ), now( )), (235, 2, 1, now( ), now( )), (236, 67, 1, now( ), now( )),
-  (237, 57, 1, now( ), now( )), (238, 58, 1, now( ), now( )), (239, 59, 1, now( ), now( )),
-  (240, 60, 1, now( ), now( )), (241, 61, 1, now( ), now( )), (242, 62, 1, now( ), now( )),
-  (243, 63, 1, now( ), now( )), (244, 64, 1, now( ), now( )), (245, 66, 1, now( ), now( )),
-  (246, 56, 1, now( ), now( )), (247, 55, 1, now( ), now( )), (248, 54, 1, now( ), now( )),
-  (249, 46, 1, now( ), now( )), (250, 47, 1, now( ), now( )), (251, 48, 1, now( ), now( )),
-  (252, 49, 1, now( ), now( )), (253, 50, 1, now( ), now( )), (254, 51, 1, now( ), now( )),
-  (255, 52, 1, now( ), now( )), (256, 53, 1, now( ), now( )), (257, 68, 1, now( ), now( ));
+VALUES (234, 1, 1, now( ), now( )), (236, 67, 1, now( ), now( )), (237, 57, 1, now( ), now( )),
+  (238, 58, 1, now( ), now( )), (239, 59, 1, now( ), now( )), (240, 60, 1, now( ), now( )),
+  (241, 61, 1, now( ), now( )), (242, 62, 1, now( ), now( )), (243, 63, 1, now( ), now( )),
+  (244, 64, 1, now( ), now( )), (245, 66, 1, now( ), now( )), (246, 56, 1, now( ), now( )),
+  (247, 55, 1, now( ), now( )), (248, 54, 1, now( ), now( )), (249, 46, 1, now( ), now( )),
+  (250, 47, 1, now( ), now( )), (251, 48, 1, now( ), now( )), (252, 49, 1, now( ), now( )),
+  (253, 50, 1, now( ), now( )), (254, 51, 1, now( ), now( )), (255, 52, 1, now( ), now( )),
+  (256, 53, 1, now( ), now( )), (257, 68, 1, now( ), now( )), (258, 69, 1, now( ), now( )),
+  (259, 70, 1, now( ), now( )), (260, 71, 1, now( ), now( ));
